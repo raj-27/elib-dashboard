b6a5f90a275e16bf503b3b1d312f500412a59690
Raj <raj819314@gmail.com>
2024-05-02 15:26:14 +0530

    Add Toaster component

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
 import './index.css';
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
+            <Toaster />
             <RouterProvider router={router} />
         </QueryClientProvider>
     </React.StrictMode>
