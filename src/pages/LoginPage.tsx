0c8eaf80e3a770e0d28fa8ecf56d53c78e08ca3b
Raj <raj819314@gmail.com>
2024-05-02 13:08:23 +0530

    Add tanstackquey and axios

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,9 +9,21 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = async () => {
+        const email = emailRef.current?.value;
+        const password = passwordRef.current?.value;
+        if (!email || !password) {
+            console.log('Field is missing');
+        }
+    };
+
     return (
         <section className="flex justify-center items-center h-screen">
             <Card className="w-full max-w-sm">
@@ -25,6 +37,7 @@ const LoginPage = () => {
                     <div className="grid gap-2">
                         <Label htmlFor="email">Email</Label>
                         <Input
+                            ref={emailRef}
                             id="email"
                             type="email"
                             placeholder="m@example.com"
@@ -33,12 +46,19 @@ const LoginPage = () => {
                     </div>
                     <div className="grid gap-2">
                         <Label htmlFor="password">Password</Label>
-                        <Input id="password" type="password" required />
+                        <Input
+                            ref={passwordRef}
+                            id="password"
+                            type="password"
+                            required
+                        />
                     </div>
                 </CardContent>
                 <CardFooter>
                     <div className="w-full">
-                        <Button className="w-full">Sign in</Button>
+                        <Button className="w-full" onClick={handleSubmit}>
+                            Sign in
+                        </Button>
                         <div className="mt-4 text-center text-sm">
                             Don't have an account?
                             <Link to={'/auth/register'} className="underline">
