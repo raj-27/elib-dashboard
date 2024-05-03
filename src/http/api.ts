4eb133ab4cd4b62d141ddb9bd67b009a0f2161e6
Raj <raj819314@gmail.com>
2024-05-03 15:58:37 +0530

    show book table

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,6 +1,13 @@
 import { config } from '@/config/config';
 import axios from 'axios';
 
+/* The `const api = axios.create({})` statement is creating an instance of Axios with specific
+configuration options. In this case:
+- `baseURL: config.baseUrl` sets the base URL for all requests made with this Axios instance. It
+allows you to define a base URL that will be prepended to all relative URLs.
+- `headers: { 'Content-Type': 'application/json' }` sets the default headers for all requests made
+with this Axios instance. In this case, it sets the 'Content-Type' header to 'application/json',
+indicating that the data being sent in the request body is in JSON format. */
 const api = axios.create({
     baseURL: config.baseUrl,
     headers: {
@@ -8,10 +15,25 @@ const api = axios.create({
     },
 });
 
+/**
+ * The login function in TypeScript sends a POST request to the 'api/user/login' endpoint with the
+ * provided email and password data.
+ * @param data - The `data` parameter in the `login` function is an object with two properties:
+ * @returns The `login` function is returning the result of a POST request to the 'api/user/login'
+ * endpoint with the provided `data` object containing an email and password.
+ */
 export const login = async (data: { email: string; password: string }) => {
     return api.post('api/user/login ', data);
 };
 
+/**
+ * The `register` function in TypeScript sends a POST request to the 'api/user/register' endpoint with
+ * user registration data.
+ * @param data - The `data` parameter in the `register` function is an object with the following
+ * properties:
+ * @returns The `register` function is returning the result of a POST request to the
+ * 'api/user/register' endpoint with the provided `data` object containing email, password, and name.
+ */
 export const register = async (data: {
     email: string;
     password: string;
@@ -19,3 +41,8 @@ export const register = async (data: {
 }) => {
     return api.post('api/user/register', data);
 };
+
+/**
+ * The function `getBooks` makes an asynchronous API call to retrieve a list of books.
+ */
+export const getBooks = async () => api.get('/api/books');
--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,5 +1,160 @@
+import { getBooks } from '@/http/api';
+import { useQuery } from '@tanstack/react-query';
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import { Link } from 'react-router-dom';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { MoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Badge } from '@/components/ui/badge';
+import { Book } from '@/type';
+
 const BookPage = () => {
-    return <div>BookPage</div>;
+    const { data, isError, isLoading } = useQuery({
+        queryKey: ['books'],
+        queryFn: getBooks,
+        staleTime: 10000,
+    });
+    return (
+        <div>
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink>
+                            <Link to={'/dashboard/home'}>Home</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <Card x-chunk="dashboard-06-chunk-0" className="mt-6">
+                <CardHeader>
+                    <CardTitle>Products</CardTitle>
+                    <CardDescription>
+                        Manage your products and view their sales performance.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="hidden w-[100px] sm:table-cell">
+                                    <span className="sr-only">Image</span>
+                                </TableHead>
+                                <TableHead>Name</TableHead>
+                                <TableHead>Status</TableHead>
+                                <TableHead className="hidden md:table-cell">
+                                    Total Sales
+                                </TableHead>
+                                <TableHead className="hidden md:table-cell">
+                                    Created at
+                                </TableHead>
+                                <TableHead>
+                                    <span className="sr-only">Actions</span>
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {data?.data?.map((book: Book) => (
+                                <TableRow>
+                                    <TableCell className="hidden sm:table-cell">
+                                        <img
+                                            alt={book.title}
+                                            className="aspect-square rounded-md object-cover"
+                                            height="64"
+                                            src={book.coverImage}
+                                            width="64"
+                                        />
+                                    </TableCell>
+                                    <TableCell className="font-medium">
+                                        {book.title}
+                                    </TableCell>
+                                    <TableCell>
+                                        <Badge variant="outline">
+                                            {book.genre}
+                                        </Badge>
+                                    </TableCell>
+                                    <TableCell className="hidden md:table-cell">
+                                        {book.author.name}
+                                    </TableCell>
+                                    <TableCell className="hidden md:table-cell">
+                                        2023-07-12 10:42 AM
+                                        {book.createdAt}
+                                    </TableCell>
+                                    <TableCell>
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger asChild>
+                                                <Button
+                                                    aria-haspopup="true"
+                                                    size="icon"
+                                                    variant="ghost">
+                                                    <MoreHorizontal className="h-4 w-4" />
+                                                    <span className="sr-only">
+                                                        Toggle menu
+                                                    </span>
+                                                </Button>
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent align="end">
+                                                <DropdownMenuLabel>
+                                                    Actions
+                                                </DropdownMenuLabel>
+                                                <DropdownMenuItem>
+                                                    Edit
+                                                </DropdownMenuItem>
+                                                <DropdownMenuItem>
+                                                    Delete
+                                                </DropdownMenuItem>
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </CardContent>
+                <CardFooter>
+                    <div className="text-xs text-muted-foreground">
+                        Showing <strong>1-10</strong> of <strong>32</strong>{' '}
+                        products
+                    </div>
+                </CardFooter>
+            </Card>
+        </div>
+    );
 };
 
 export default BookPage;
